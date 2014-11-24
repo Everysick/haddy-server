237b9233697c3413a142ec2815b30e3f29af2c6f
everysick <s.wakeup31@gmail.com>
2014-11-25 04:44:57 +0900

    Added token generater

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
+# coding: utf-8
 class User < ActiveRecord::Base
+  before_save :ensure_authentication_token
   has_many :user_post
   has_many :user_auth_token
   
@@ -6,4 +8,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+
+  private
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
+  
 end
